4d00d269a0cd47b70c06a354caa2b62c8aa27e8f
Hector Santiago <52340742+santiago0072002@users.noreply.github.com>
2022-02-21 22:33:10 -0500

    Add files via upload
    This is just a small script that points to different OSINT places to help with the OSINT process.

---
+++ osint.php
@@ -0,0 +1,111 @@
+<style>
+    .button {
+        background-color: #4CAF50;
+        /* Green */
+        border: none;
+        color: white;
+        padding: 16px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        transition-duration: 0.4s;
+        cursor: pointer;
+    }
+</style>
+
+
+<?php
+
+
+function MyDirectories($dir){
+    
+    
+    $dir = ".";
+    // Sort in descending order
+    $b = scandir($dir, 1);
+    echo 'In ' . getcwd() . ' the current dirrectory we have: <br>';
+    // print_r($a);
+    echo '<pre>';
+    print_r($b);
+    // $b = chdir("../../");
+    $dir = $b;
+    // Sort in descending order
+    // $b = scandir($dir, 1);
+    echo '<pre>';
+    // print_r($b);
+ 
+
+
+    
+}
+
+
+MyDirectories(".");
+
+   ?>
+
+
+    <html>
+
+    <button class="" style="width:20%" onclick="window.location.href='https://www.fakenamegenerator.com/';"> SOCK ACCOUNT NAME</button><label> From fake name generator you can create a person that never have existed with Name, address, mother’s Maiden name, Address, Email address, weight, height, credit card, date of birth, favorite color, 
+    vehicle number and all sorts of information you need to be that person</label>
+    <pre></pre>
+    <label>General Search/ Main search engines used by Users</label>
+
+    <button class="" style="width:20%" onclick="window.location.href='https://github.com/jivoi/awesome-osint';">Jivoi Awesome Github</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='https://osintframework.com/';">OSINT FRAMEWORK</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://advangle.com/';"> Advangle</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://search.aol.com/';"> AOL</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.ask.com/';"> ASK</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='https://duckduckgo.com/';"> Duck Duck Go</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://gigablast.com/ ';">Gigablast</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.goodsearch.com/';">Good search</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.google.com/';">Google Search</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.instya.com/ ';">Instya</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.impersonal.me/';">Impersonal me</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://education.iseek.com/iseek/home.page';">i seek education</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='https://www.ixquick.com/';">ixquick</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.lycos.com/';">lycos</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.search.com/';">Search.com</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.surfcanyon.com/';">Surf Cannon</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.teoma.com/';">Teoma</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.wolframalpha.com/';">Wolfram Alpha</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.yahoo.com/';">Yahoo Search</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://all-io.net/';">All in one</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.alltheinternet.com/';">All in the internet</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.etools.ch/';">Etools</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.faganfinder.com/engines';">Fagan Finder</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.goofram.com/';">Goofram</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.izito.com/';">izito</button>
+
+    <button class="" style="width:20%" onclick="window.location.href='http://www.nextaris.com/';">Nextaris</button>
+
+    
+
+    </html>
